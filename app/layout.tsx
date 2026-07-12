6e9b5074f2a3d4f769c3862af52bdf5078474cb8
dmitriy <kdv41rus@yandex.ru>
2026-07-12 12:58:33 +0300

    update favicon icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,8 +76,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
-      { url: '/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },     
     ],
     apple: '/apple-touch-icon.png',
   },
